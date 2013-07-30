9e794a8652e5c441dde8501cc323a4fb35aa8f72
Pete Bachant <petebachant@gmail.com>
2013-07-29 20:46:09 -0400

    Rearrange classes

--- callscript.py
+++ callscript.py
@@ -7,19 +7,16 @@ Created on Fri Jul 26 19:33:00 2013
 This script tests the ability to call MakeWaves functionality from other
 applications.
 
-It works, but the calls are a bit awkward. Classes should be rearranged a bit.
-
 """
 
 import makewaves
 import time
 
 # Create a wave generation object
-wavegen = makewaves.MainWindow.WaveGen("Regular")
-wavegen.wave.height = 0.1
+wavegen = makewaves.WaveGen(makewaves.JONSWAP)
+wavegen.wave.sig_height = 0.1
 wavegen.start()
 
 time.sleep(4)
 
-wavestop = makewaves.MainWindow.WaveStop(wavegen)
-wavestop.start()
+wavegen.stop()
--- makewaves.py
+++ makewaves.py
@@ -16,6 +16,14 @@ import daqmx
 import time
 from wavetsgen import Wave, ramp_ts
 
+# Constants
+REGULAR = "Regular"
+BRETSCHNEIDER = "Bretschneider"
+JONSWAP = "JONSWAP"
+NH_EXTREME = "NH Extreme"
+NH_TYPICAL = "NH Typical"
+PIERSON_MOSCOWITZ = "Pierson-Moscowitz"
+
 
 # Spectral parameters for random waves
 bret_params = [("Significant Wave Height", 0.1),
@@ -285,7 +293,7 @@ class MainWindow(QtGui.QMainWindow):
                 self.period = self.ui.spinbox_wave_period.value()
                 self.height = self.ui.spinbox_wave_height.value()
                 
-                self.wavegen = self.WaveGen("Regular")
+                self.wavegen = WaveGen("Regular")
                 self.wavegen.wave.period = self.period
                 self.wavegen.wave.height = self.height
                 self.wavegen.start()
@@ -295,7 +303,7 @@ class MainWindow(QtGui.QMainWindow):
                 """Create random waves."""
                 rspec = self.ui.combobox_randwavetype.currentText()
                 self.slabel.setText("Generating " + rspec + " waves... ")
-                self.wavegen = self.WaveGen(rspec)
+                self.wavegen = WaveGen(rspec)
                 self.wavegen.start()
                 
             self.timer.start(500)              
@@ -304,9 +312,8 @@ class MainWindow(QtGui.QMainWindow):
         elif self.ui.action_start.isChecked() == False:
             """Stop making waves"""
             self.slabel.setText("Stopping... ")
-            self.wavestop = self.WaveStop(self.wavegen)
-            self.wavestop.finished.connect(self.on_wave_finished)
-            self.wavestop.start()
+            self.wavegen.stop()
+            self.wavegen.stopgen.finished.connect(self.on_wave_finished)
             self.ui.action_start.setEnabled(False)
 
             
@@ -340,139 +347,137 @@ class MainWindow(QtGui.QMainWindow):
                 self.wavestop.start()
                 
                 
-    class WaveGen(QtCore.QThread):
-        def __init__(self, wavetype):
-            QtCore.QThread.__init__(self)
-            self.wavetype = wavetype
-            self.wave = Wave(self.wavetype)
-            self.enable = True
-            
-        def run(self):
-            self.rampeddown = False
-            self.cleared = False
-            self.making = False
-            
-            # Compute the voltage time series associated with the wave
-            self.wave.gen_ts_volts()
-            
-            # Get parameters from the wave object
-            self.period = self.wave.period
-            self.height = self.wave.height
-            self.buffsize = self.wave.sbuffsize
-            self.sr = self.wave.sr
-            
-            # Get data to write from the wave object
-            self.ts_plot = self.wave.ts_elev
-            self.dataw = self.wave.ts_volts
-            
-            # If random waves, divide up time series into 64 parts
-            if self.wavetype != "Regular":
-                tsparts = np.zeros((64, 1024))
-                for n in range(64):
-                    tsparts[n, :] = self.dataw[n*1024:(n+1)*1024]
-                    
-                self.dataw = tsparts[0, :]
-            
-            # Compute spectrum for plot
-            self.outf, self.outspec = self.wave.comp_spec()
-            
-            # Ramp time series
-            rampup_ts = ramp_ts(self.dataw, "up")
-            
-            # Set making variable true
-            self.making = True
-            
-            self.AOtaskHandle = daqmx.TaskHandle()
-            daqmx.CreateTask("", self.AOtaskHandle)
-            daqmx.CreateAOVoltageChan(self.AOtaskHandle, "Dev1/ao0", "", 
-                                      -5.0, 5.0, daqmx.Val_Volts, None)
-            daqmx.CfgSampClkTiming(self.AOtaskHandle, "", self.sr, 
-                                   daqmx.Val_Rising, daqmx.Val_ContSamps, 
-                                   self.buffsize)
-                                   
-            daqmx.SetWriteRegenMode(self.AOtaskHandle, 
-                                    daqmx.Val_DoNotAllowRegen)
-                                    
-            # Setup a callback function to run once the DAQmx driver finishes
-            def DoneCallback_py(taskHandle, status, callbackData_ptr):
-                self.rampeddown = True
-                print "Done"
-                return 0
+class WaveGen(QtCore.QThread):
+    def __init__(self, wavetype):
+        QtCore.QThread.__init__(self)
+        self.wavetype = wavetype
+        self.wave = Wave(self.wavetype)
+        self.enable = True
+        
+    def run(self):
+        self.rampeddown = False
+        self.cleared = False
+        self.making = False
+        
+        # Compute the voltage time series associated with the wave
+        self.wave.gen_ts_volts()
+        
+        # Get parameters from the wave object
+        self.period = self.wave.period
+        self.height = self.wave.height
+        self.buffsize = self.wave.sbuffsize
+        self.sr = self.wave.sr
+        
+        # Get data to write from the wave object
+        self.ts_plot = self.wave.ts_elev
+        self.dataw = self.wave.ts_volts
+        
+        # If random waves, divide up time series into 64 parts
+        if self.wavetype != "Regular":
+            tsparts = np.zeros((64, 1024))
+            for n in range(64):
+                tsparts[n, :] = self.dataw[n*1024:(n+1)*1024]
                 
-            DoneCallback = daqmx.DoneEventCallbackPtr(DoneCallback_py)
-            daqmx.RegisterDoneEvent(self.AOtaskHandle, 0, DoneCallback, None)                        
+            self.dataw = tsparts[0, :]
+        
+        # Compute spectrum for plot
+        self.outf, self.outspec = self.wave.comp_spec()
+        
+        # Ramp time series
+        rampup_ts = ramp_ts(self.dataw, "up")
+        
+        # Set making variable true
+        self.making = True
+        
+        self.AOtaskHandle = daqmx.TaskHandle()
+        daqmx.CreateTask("", self.AOtaskHandle)
+        daqmx.CreateAOVoltageChan(self.AOtaskHandle, "Dev1/ao0", "", 
+                                  -5.0, 5.0, daqmx.Val_Volts, None)
+        daqmx.CfgSampClkTiming(self.AOtaskHandle, "", self.sr, 
+                               daqmx.Val_Rising, daqmx.Val_ContSamps, 
+                               self.buffsize)
+                               
+        daqmx.SetWriteRegenMode(self.AOtaskHandle, 
+                                daqmx.Val_DoNotAllowRegen)
+                                
+        # Setup a callback function to run once the DAQmx driver finishes
+        def DoneCallback_py(taskHandle, status, callbackData_ptr):
+            self.rampeddown = True
+            return 0
             
-            # Output the rampup time series
-            daqmx.WriteAnalogF64(self.AOtaskHandle, self.buffsize, False, 10.0, 
-                                 daqmx.Val_GroupByChannel, rampup_ts)
-                                           
-            daqmx.StartTask(self.AOtaskHandle)
-            
-            # Wait a couple seconds to allow the DAQmx buffer to empty
-            time.sleep(self.period*0.9)
+        DoneCallback = daqmx.DoneEventCallbackPtr(DoneCallback_py)
+        daqmx.RegisterDoneEvent(self.AOtaskHandle, 0, DoneCallback, None)                        
+        
+        # Output the rampup time series
+        daqmx.WriteAnalogF64(self.AOtaskHandle, self.buffsize, False, 10.0, 
+                             daqmx.Val_GroupByChannel, rampup_ts)
+                                       
+        daqmx.StartTask(self.AOtaskHandle)
+        
+        # Wait a couple seconds to allow the DAQmx buffer to empty
+        time.sleep(self.period*0.9)
 
-            # Set iteration variable to keep track of how many chunks of data
-            # have been written
-            i = 1
+        # Set iteration variable to keep track of how many chunks of data
+        # have been written
+        i = 1
+        
+        # Main running loop that writes data to DAQmx buffer
+        while self.enable:
+            writeSpaceAvail = daqmx.GetWriteSpaceAvail(self.AOtaskHandle)
+#            print "Available:", writeSpaceAvail
             
-            # Main running loop that writes data to DAQmx buffer
-            while self.enable:
-                writeSpaceAvail = daqmx.GetWriteSpaceAvail(self.AOtaskHandle)
-                print "Available:", writeSpaceAvail
+            if writeSpaceAvail >= self.buffsize:
                 
-                if writeSpaceAvail >= self.buffsize:
-                    
-                    if self.wavetype != "Regular":
-                        if i >= 64:
-                            self.dataw = tsparts[i % 64, :]
-                        else: 
-                            self.dataw = tsparts[i, :]
-                    
-                    w = daqmx.WriteAnalogF64(self.AOtaskHandle, self.buffsize, False, 10.0, 
-                                             daqmx.Val_GroupByChannel, self.dataw)
-                    print "Wrote", w
-                    print "Iteration:", i
-                    i += 1
-                    
-                time.sleep(self.period)
-            
-            # After disabled, initiate rampdown timeseries
-            if self.wavetype != "Regular":
-                if i >= 64:
-                    self.rampdown_ts = ramp_ts(tsparts[i % 64, :], "down")
-                else:
-                    self.rampdown_ts = ramp_ts(tsparts[i, :], "down")
+                if self.wavetype != "Regular":
+                    if i >= 64:
+                        self.dataw = tsparts[i % 64, :]
+                    else: 
+                        self.dataw = tsparts[i, :]
+                
+                w = daqmx.WriteAnalogF64(self.AOtaskHandle, self.buffsize, False, 10.0, 
+                                         daqmx.Val_GroupByChannel, self.dataw)
+#                print "Wrote", w
+#                print "Iteration:", i
+                i += 1
+                
+            time.sleep(self.period)
+        
+        # After disabled, initiate rampdown timeseries
+        if self.wavetype != "Regular":
+            if i >= 64:
+                self.rampdown_ts = ramp_ts(tsparts[i % 64, :], "down")
             else:
-                self.rampdown_ts = ramp_ts(self.dataw, "down")
-                
-            daqmx.WriteAnalogF64(self.AOtaskHandle, self.buffsize, False, 10.0, 
-                                     daqmx.Val_GroupByChannel, self.rampdown_ts)
+                self.rampdown_ts = ramp_ts(tsparts[i, :], "down")
+        else:
+            self.rampdown_ts = ramp_ts(self.dataw, "down")
             
-            # Keep running, part of PyDAQmx callback syntax
-            while True:
-                pass
-            
-            
-        def clear(self):
-#            daqmx.StopTask(self.AOtaskHandle)
-            daqmx.ClearTask(self.AOtaskHandle)   
-            self.cleared = True
-            print "Task cleared"
+        daqmx.WriteAnalogF64(self.AOtaskHandle, self.buffsize, False, 10.0, 
+                                 daqmx.Val_GroupByChannel, self.rampdown_ts)
+        
+        # Keep running, part of PyDAQmx callback syntax
+        while True:
+            pass
+        
+        
+    def stop(self):
+        self.stopgen = WaveStop(self)
+        self.stopgen.start()
+        
 
 
-    class WaveStop(QtCore.QThread):
-        def __init__(self, wavegen):
-            QtCore.QThread.__init__(self)
-            self.wavegen = wavegen
-            
-        def run(self):
-            self.wavegen.enable = False
-            print "Waiting to ramp down"
-            
-            while not self.wavegen.rampeddown:
-                time.sleep(0.2)
-            
-            self.wavegen.clear()
+class WaveStop(QtCore.QThread):
+    def __init__(self, wavegen):
+        QtCore.QThread.__init__(self)
+        self.wavegen = wavegen
+        
+    def run(self):
+        self.wavegen.enable = False
+        
+        while not self.wavegen.rampeddown:
+            time.sleep(0.2)
+        
+        daqmx.ClearTask(self.wavegen.AOtaskHandle)
+        self.wavegen.cleared = True
                
     
 # Boilerplate code to run a Qt application
