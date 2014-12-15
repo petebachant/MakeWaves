40e55020ce69a975fe1db3f1980f92cb8bfb70b7
Pete Bachant <petebachant@gmail.com>
2014-12-15 09:54:44 -0500

    Add test script for running without installation

---
+++ makewaves/__init__.py
@@ -0,0 +1,3 @@
+
+from . import makewaves
+from .makewaves import *
---
+++ test.py
@@ -0,0 +1,9 @@
+"""
+This script allows testing of MakeWaves without installing.
+
+"""
+
+import makewaves
+
+if __name__ == "__main__":
+    makewaves.main()
