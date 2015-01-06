5a0975f6a4b9efb007444b3b85c510d574fc8db4
Pete Bachant <petebachant@gmail.com>
2015-01-05 22:55:19 -0500

    Shortcut creation script is more modular

--- create_shortcut.py
+++ create_shortcut.py
@@ -5,12 +5,19 @@ This script creates Windows shortcuts.
 
 import os
 from win32com.client import Dispatch
+import sys
 
+python_dir = os.path.split(sys.executable)[0]
 shortcut_path = "MakeWaves.lnk"
-pythonw_path = r"C:\Anaconda\pythonw.exe"
-makewaves_path = r"C:\Anaconda\Scripts\makewaves-script.py"
+pythonw_path = os.path.join(python_dir, "pythonw.exe")
+makewaves_path = os.path.join(python_dir, "Scripts", "makewaves-script.py")
 wdir = r"C:\temp"
-icon = r"C:\Anaconda\Lib\site-packages\makewaves\icons\makewaves_icon.ico"
+icon = os.path.join(python_dir,
+                    "Lib",
+                    "site-packages",
+                    "makewaves",
+                    "icons",
+                    "makewaves_icon.ico")
 target_path = '{} {}'.format(pythonw_path, makewaves_path)
 print target_path
 
