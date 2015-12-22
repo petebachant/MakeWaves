2dc42bc732685671df0ef662c2013ae0b179fbb1
Pete Bachant <petebachant@gmail.com>
2015-12-21 22:18:56 -0500

    Add script for making ui

---
+++ makeui.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo Building GUI
+
+python -m PyQt4.uic.pyuic.py makewaves/mainwindow.ui -o tow/mainwindow.py
+
+echo Building resource file
+
+pyrcc4 -py3 makewaves/icons/resources.qrc -o makewaves/resources_rc.py
+
+echo Replacing relative import
+
+sed -i 's/import resources_rc/from . import resources_rc/g' makewaves/mainwindow.py
+
+echo Done
