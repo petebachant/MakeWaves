47216d858422601d8bf1ba8786f77a3016cafb6b
Pete Bachant <petebachant@gmail.com>
2015-12-21 22:32:38 -0500

    Use relative imports

--- makewaves/makewaves.py
+++ makewaves/makewaves.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Jun 02 08:53:07 2013
+"""MakeWaves main application."""
 
-@author: Pete
-
-"""
 from __future__ import division, print_function
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4 import QtGui
-from mainwindow import *
+from .mainwindow import *
 import PyQt4.Qwt5 as Qwt
 import sys
 import os
 import platform
-import wavemakerlimits as wml
+from . import wavemakerlimits as wml
 import numpy as np
 import time
 import json
-from waveio import WaveGen
+from .waveio import WaveGen
 
 _thisdir = sys.prefix + "/Lib/site-packages/makewaves"
 
@@ -31,14 +27,14 @@ else:
 bret_params = [("Significant Wave Height", 0.1),
                ("Significant Wave Period", 1.0),
                ("Scale Ratio", 1.0)]
-               
+
 jonswap_params = [("Significant Wave Height", 0.1),
                   ("Significant Wave Period", 1.0),
                   ("Scale Ratio", 1.0),
                   ("Gamma", 3.3),
                   ("Sigma A", 0.07),
                   ("Sigma B", 0.09)]
-                  
+
 nhextreme_params = [("Significant Wave Height", 6.58),
                     ("Significant Wave Period", 10.5),
                     ("Scale Ratio", 15.2),
@@ -46,7 +42,7 @@ nhextreme_params = [("Significant Wave Height", 6.58),
                     ("Sigma A", 0.45),
                     ("Sigma B", 0.15),
                     ("P", 4.85)]
-                    
+
 nhtypical_params = [("Significant Wave Height", 1.21),
                     ("Significant Wave Period 1", 10.0),
                     ("Significant Wave Period 2", 5.34),
@@ -54,15 +50,15 @@ nhtypical_params = [("Significant Wave Height", 1.21),
                     ("Gamma 1", 6.75),
                     ("Gamma 2", 0.5),
                     ("P", 4.34)]
-                    
+
 pm_params = [("Wind Speed", 2.0),
              ("Scale Ratio", 1.0)]
-             
+
 rw_params = {"Bretschneider" : bret_params,
              "JONSWAP" : jonswap_params,
              "Pierson-Moskowitz" : pm_params}
-                  
-             
+
+
 # Some universal constants
 paddle_height = 1.0
 water_depth = 2.44
@@ -86,7 +82,7 @@ maxperiod_rand = 3.5
 cutoff_freq = 2.0
 max_H_L_rand = 0.07
 max_H_d_rand = 0.50
-    
+
 
 class MainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
@@ -95,48 +91,48 @@ class MainWindow(QtGui.QMainWindow):
         self.ui.setupUi(self)
         self.wavegen = None
         self.calcthread = None
-        
+
         # Load settings is they exist
         self.load_settings()
-        
+
         # Add a label to the status bar
         self.slabel = QLabel()
         self.ui.statusbar.addWidget(self.slabel)
         self.slabel.setText("Stopped ")
-        
+
         # Set up a timer
         self.timer = QTimer()
         self.timer.timeout.connect(self.on_timer)
-        
+
         # Set up a control variable for parameters
         self.parameters = "HT"
-        
+
         # Set default tab to regular waves
         self.ui.tabwidget.setCurrentIndex(0)
-        
+
         # Initialize slider values
         hmax = maxH[np.where(periods==np.round(1, decimals=2))[0]]
         self.ui.slider_height.setRange(0.0, hmax, 0.001, 10)
         self.ui.spinbox_wave_height.setMaximum(hmax)
         self.ui.slider_height.setValue(self.ui.spinbox_wave_height.value())
         self.ui.slider_height.setScaleMaxMajor(12)
-        
+
         self.ui.slider_horiz.setRange(0.5, 5, 0.001, 10)
         self.ui.slider_horiz.setScaleMaxMajor(12)
         self.ui.slider_horiz.setValue(self.ui.spinbox_wave_period.value())
-        
+
         # Initialize wavelength value
         wl = 2*np.pi/wml.dispsolver(2*np.pi/1.0, water_depth, decimals=2)
         self.ui.spinbox_wavelength.setValue(wl)
-        
+
         # Initialize plot settings
         self.initialize_plots()
-        self.connectslots()        
+        self.connectslots()
         # Add dock widgets to right dock widget area and tabify them
         self.tabifyDockWidget(self.ui.dock_time_series,
-                              self.ui.dock_spectrum)                              
+                              self.ui.dock_spectrum)
         self.on_rw_changed()
-        
+
     def load_settings(self):
         """Loads settings"""
         self.pcid = platform.node()
@@ -150,14 +146,14 @@ class MainWindow(QtGui.QMainWindow):
                 if "Last window location" in self.settings:
                     self.move(QtCore.QPoint(self.settings["Last window location"][0],
                                             self.settings["Last window location"][1]))
-        
-    def setup_spinboxes(self, nboxes):  
+
+    def setup_spinboxes(self, nboxes):
         """Add double spin boxes to the random waves table widget"""
         self.spinboxes_rw = []
         for n in range(nboxes):
             self.spinboxes_rw.append(QDoubleSpinBox())
             self.ui.table_rwaves.setCellWidget(n, 1, self.spinboxes_rw[n])
-    
+
     def initialize_plots(self):
         # Create time series plot
         self.plot_ts = self.ui.plot_ts
@@ -177,7 +173,7 @@ class MainWindow(QtGui.QMainWindow):
         self.pen.setWidth(1.5)
         self.curve_ts.setPen(self.pen)
         self.curve_ts.attach(self.plot_ts)
-        
+
         # Create output spectrum plot
         self.plot_spec = self.ui.plot_spec
         self.plot_spec.setCanvasBackground(Qt.white)
@@ -194,7 +190,7 @@ class MainWindow(QtGui.QMainWindow):
         self.pen.setWidth(1.5)
         self.curve_spec.setPen(self.pen)
         self.curve_spec.attach(self.plot_spec)
-        
+
     def connectslots(self):
         self.ui.spinbox_wave_height.valueChanged.connect(self.on_wh_changed)
         self.ui.spinbox_wave_period.valueChanged.connect(self.on_wp_changed)
@@ -210,14 +206,14 @@ class MainWindow(QtGui.QMainWindow):
         self.ui.dock_spectrum.visibilityChanged.connect(self.ui.action_view_spec.setChecked)
         self.ui.action_view_spec.triggered.connect(self.ui.dock_spectrum.setVisible)
         self.ui.dock_time_series.visibilityChanged.connect(self.on_dock_ts)
-        
+
     def on_dock_ts(self):
         if self.ui.dock_time_series.isHidden():
             self.ui.action_view_ts.setChecked(False)
-    
+
     def on_timer(self):
         self.update_plot()
-        
+
     def update_plot(self):
         # Plot output time series
         if self.wavegen.making:
@@ -225,18 +221,18 @@ class MainWindow(QtGui.QMainWindow):
             xdata = np.asarray(np.arange(len(ydata))/self.wavegen.sr)
             self.plot_ts.setAxisScale(Qwt.QwtPlot.xBottom, 0, xdata[-1])
             self.curve_ts.setData(xdata, ydata)
-            self.plot_ts.replot()    
+            self.plot_ts.replot()
         if self.wavegen.making:
             ydata = self.wavegen.outspec
             xdata = self.wavegen.outf
             self.plot_spec.setAxisScale(Qwt.QwtPlot.xBottom, 0, 5)
             self.curve_spec.setData(xdata, ydata)
             self.plot_spec.replot()
-        
+
     def on_wh_changed(self):
         # Need some way to let user continue typing before changing slider
         self.ui.slider_height.setValue(self.ui.spinbox_wave_height.value())
-        
+
     def on_wp_changed(self):
         wp = self.ui.spinbox_wave_period.value()
         if self.parameters == "HT":
@@ -255,17 +251,17 @@ class MainWindow(QtGui.QMainWindow):
             hmax = maxH[np.where(periods==np.round(wp, decimals=2))[0][0]]
             self.ui.spinbox_wave_height.setMaximum(hmax)
             self.ui.slider_height.setRange(0, hmax, 0.001, 10)
-            self.ui.slider_horiz.setValue(wl)            
-        
+            self.ui.slider_horiz.setValue(wl)
+
     def on_slider_height(self):
         self.ui.spinbox_wave_height.setValue(self.ui.slider_height.value())
-        
+
     def on_slider_horiz(self):
         if self.parameters == "HT":
             self.ui.spinbox_wave_period.setValue(self.ui.slider_horiz.value())
         elif self.parameters == "HL":
             self.ui.spinbox_wavelength.setValue(self.ui.slider_horiz.value())
-        
+
     def on_regpars(self):
         """Decides which two parameters to use for regular waves"""
         if self.ui.combobox_regparams.currentIndex() == 0:
@@ -280,9 +276,9 @@ class MainWindow(QtGui.QMainWindow):
             self.ui.spinbox_wavelength.setMaximum(30)
             self.ui.spinbox_wave_period.setMaximum(5)
             self.ui.spinbox_wave_period.setMinimum(0.5)
-            
+
         elif self.ui.combobox_regparams.currentIndex() == 1:
-            self.parameters = "HL"  
+            self.parameters = "HL"
             wp = self.ui.spinbox_wave_period.value()
             wl = 2*np.pi/wml.dispsolver(2*np.pi/wp, water_depth, decimals=1)
             self.ui.spinbox_wave_period.setEnabled(False)
@@ -291,7 +287,7 @@ class MainWindow(QtGui.QMainWindow):
             self.ui.slider_horiz.setValue(wl)
             self.ui.spinbox_wavelength.setMaximum(maxL)
             self.ui.spinbox_wavelength.setMinimum(minL)
-            
+
     def on_rw_changed(self):
         self.rw_type = str(self.ui.combobox_randwavetype.currentText())
         self.rw_params = rw_params[self.rw_type]
@@ -313,7 +309,7 @@ class MainWindow(QtGui.QMainWindow):
             row += 1
         for sb in self.spinboxes_rw:
             sb.valueChanged.connect(self.on_rw_param_changed)
-            
+
     def on_rw_param_changed(self):
         """If a random wave parameter is changed, start a thread to check
             if the parameters don't over-extend the piston."""
@@ -347,7 +343,7 @@ class MainWindow(QtGui.QMainWindow):
                 self.wavegen.wave.period = self.period
                 self.wavegen.wave.height = self.height
                 self.wavegen.start()
-                
+
             elif wavetype == 1:
                 """Create random waves."""
                 rspec = self.ui.combobox_randwavetype.currentText()
@@ -361,8 +357,8 @@ class MainWindow(QtGui.QMainWindow):
                     self.wavegen.wave.windspeed = self.spinboxes_rw[0].value()
                     self.wavegen.wave.scale_ratio = self.spinboxes_rw[1].value()
                 self.wavegen.start()
-            self.timer.start(500)              
-                
+            self.timer.start(500)
+
         elif self.ui.action_start.isChecked() == False:
             """Stop making waves"""
             self.slabel.setText("Stopping... ")
@@ -379,18 +375,18 @@ class MainWindow(QtGui.QMainWindow):
         self.ui.action_start.setIcon(QIcon(":icons/play.png"))
         self.ui.action_start.setEnabled(True)
         self.ui.tabwidget.setEnabled(True)
-        
+
     def on_about(self):
         about_text = QString("<b>MakeWaves 0.0.1</b><br>")
         about_text.append("A wavemaking app for the UNH tow/wave tank<br><br>")
         about_text.append("Created by Pete Bachant (petebachant@gmail.com)<br>")
         about_text.append("with contributions by Toby Dewhurst and Matt Rowell.")
         QMessageBox.about(self, "About MakeWaves", about_text)
-        
+
     def on_wiki(self):
         url = QUrl("http://marine.unh.edu/oelab/wiki/doku.php?id=tow_tank:operation:wavemaker")
         QDesktopServices.openUrl(url)
-        
+
     def closeEvent(self, event):
         if self.wavegen != None:
             if self.wavegen.isRunning() and not self.wavegen.cleared:
@@ -411,19 +407,19 @@ class MainWindow(QtGui.QMainWindow):
                     progress += int(60*0.1/self.wavegen.period)
                     pbar.setValue(progress)
                 dialog.close()
-        self.settings["Last window location"] = [self.pos().x(), 
+        self.settings["Last window location"] = [self.pos().x(),
                                                  self.pos().y()]
         self.settings["Last PC name"] = self.pcid
         with open(_thisdir + "settings/app.json", "w") as fn:
             json.dump(self.settings, fn, indent=4)
 
-        
+
 class CalcThread(QThread):
     def __init__(self, main_window, wave):
         QtCore.QThread.__init__(self)
         self.mw = main_window
         self.wave = wave
-        
+
     def run(self):
         self.wave.gen_ts_stroke()
         if self.wave.ts_stroke.max() > wml.max_halfstroke:
@@ -433,17 +429,15 @@ class CalcThread(QThread):
 
 # Boilerplate code to run a Qt application
 def main():
-    
+
     app = QtGui.QApplication(sys.argv)
 
     w = MainWindow()
     w.setWindowIcon(QtGui.QIcon(_thisdir + "icons/makewaves_icon.svg"))
     w.show()
-    
+
     sys.exit(app.exec_())
-    
+
 # Boilerplate code to run the Python application
 if __name__ == '__main__':
     main()
-
-
